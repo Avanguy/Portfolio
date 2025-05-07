b30f1743140424b645b03af92d3a297e2c4d4e87
Avanguy <nguyenvalbert@gmail.com>
2025-05-07 10:34:47 +0100

    feat: Add tech stack sections and enhance project descriptions in Projects component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="hero" id="hero">
       <div className="container">
         <h1>Hey there! I'm Albert.</h1>
-        <p>I build fast, functional, and user-focused web apps — full stack and always learning.</p>
+        <p>I build fast, functional, and user-focused web apps. <br/> Full stack and always learning.</p>
         <div className="hero-buttons">
           <button className="btn" onClick={() => window.location.href="#gachess"}>See My Work</button>
           <button className="btn" onClick={() => window.location.href="#about"}>About Me</button>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,21 +10,54 @@ const Projects = () => {
           <img src="https://via.placeholder.com/150" alt="Budget Tracker Image" />
           <div>
             <h3>Budget Tracker</h3>
-            <p>Description of project 1.</p>
-          </div>
-        </div>
-        <div className="projectItem slideRight">
-          <img src="https://via.placeholder.com/150" alt="Weather App Image" />
-          <div>
-            <h3>Weather</h3>
-            <p>Description of project 1.</p>
+            <p>A full-stack MERN project that allows users to manage their personal finances by tracking income and expenses. 
+            <br/><br/>The app provides real-time data updates, user authentication, and a dynamic UI with graphs to visualize spending patterns.</p>
+            <div>
+              <ul className="tech-stack">
+                <li><i className="fab fa-html5"></i> HTML</li>
+                <li><i className="fab fa-js"></i> JavaScript</li>
+                <li><i className="fab fa-react"></i> React</li>
+                <li><i className="fas fa-paint-brush"></i> Tailwind CSS</li>
+              </ul>
+            </div>
+            <div>
+              <ul className="tech-stack">
+                <li><i className="fab fa-node"></i> Node.js</li>
+                <li><i className="fas fa-server"></i> Express</li>
+                <li><i className="fas fa-random"></i> REST APIs</li>
+                <li><i className="fas fa-lock"></i> JWT</li>
+                <li><i className="fas fa-database"></i> MongoDB</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="projectItem slideLeft">
           <img src="https://via.placeholder.com/150" alt="Todo List Image" />
           <div>
             <h3>Todo List</h3>
-            <p>Description of project 1.</p>
+            <p>A simple To-Do List built with React to learn and practice core React concepts.</p>
+            <ul className="tech-stack">
+              <li><i className="fab fa-html5"></i> HTML</li>
+              <li><i className="fab fa-css3-alt"></i> CSS</li>
+              <li><i className="fab fa-js"></i> JavaScript</li>
+              <li><i className="fab fa-react"></i> React</li>
+            </ul>
+          </div>
+        </div>
+        <div className="projectItem slideRight">
+          <img src="https://via.placeholder.com/150" alt="Weather App Image" />
+          <div>
+            <h3>Weather App</h3>
+            <p>A weather app built with vanilla JavaScript to strengthen fundamentals and work without React.</p>
+            <p>Using the OpenWeather API</p>
+            <div>
+              <ul className="tech-stack">
+                <li><i class="fab fa-html5"></i> HTML</li>
+                <li><i class="fab fa-css3-alt"></i> CSS</li>
+                <li><i class="fab fa-js"></i> JavaScript</li>
+                <li><i className="fas fa-random"></i> REST API</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
